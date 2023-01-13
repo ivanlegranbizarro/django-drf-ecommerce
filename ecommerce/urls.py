061141cfa6871e0d4a7c744a897a1913617ce87d
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-13 09:22:19 +0100

    first viewset and models registered to the admin

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from product import views
+
+router = DefaultRouter()
+router.register(r"categories", views.CategoryViewSet, basename="categories")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
