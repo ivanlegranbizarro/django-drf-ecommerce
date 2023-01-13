95784d4d0c74e3390f19a800bcd31546b5fb7e42
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-13 09:39:05 +0100

    drf_spectacular

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "product",
 ]
 
@@ -127,4 +128,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+    "DESCRIPTION": "API for ecommerce done with Django and DRF",
+    "VERSION": "1.0.1",
+}
