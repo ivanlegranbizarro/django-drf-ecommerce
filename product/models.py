273b5264ad1985f4dfc4d5c0bec1149fbb5a993f
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-14 10:46:18 +0100

    model and serializer of ProductLine

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
@@ -47,3 +48,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductLine(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sku = models.CharField(max_length=100, unique=True)
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.product.name
