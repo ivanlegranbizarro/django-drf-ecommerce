e7bf8ae1696d53a5569ee5f7f78ead6d5a49d1f1
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-15 08:18:53 +0100

    productline on django admin

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
-from .models import Brand, Category, Product
+from .models import Brand, Category, Product, ProductLine
 
 # Register your models here.
 
+
+class ProductLineInline(admin.TabularInline):
+    model = ProductLine
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInline]
+
+
 admin.site.register(Brand)
 admin.site.register(Category)
-admin.site.register(Product)
